29aa66e4c3e4b2eff8c871488b24da9b81f4d3fd
pedro alfredo angulo <anpe88@hotmail.com>
2018-04-21 14:00:34 -0500

    commit inicial

---
+++ demo-spring/src/main/java/co/edu/usbcali/demo/modelo/Motor.java
@@ -0,0 +1,37 @@
+package co.edu.usbcali.demo.modelo;
+
+public class Motor {
+
+	private String serie;
+	private Integer cilindraje;
+	private String marca;
+	private boolean electrico;
+	
+	
+	public String getSerie() {
+		return serie;
+	}
+	public void setSerie(String serie) {
+		this.serie = serie;
+	}
+	public Integer getCilindraje() {
+		return cilindraje;
+	}
+	public void setCilindraje(Integer cilindraje) {
+		this.cilindraje = cilindraje;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public boolean isElectrico() {
+		return electrico;
+	}
+	public void setElectrico(boolean electrico) {
+		this.electrico = electrico;
+	}
+	
+
+}
---
+++ demo-spring/src/main/java/co/edu/usbcali/demo/modelo/Vehiculo.java
@@ -0,0 +1,28 @@
+package co.edu.usbcali.demo.modelo;
+
+public class Vehiculo {
+
+	private Motor motor;
+	private String color;
+	private String marca;
+	
+	public Motor getMotor() {
+		return motor;
+	}
+	public void setMotor(Motor motor) {
+		this.motor = motor;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+}
